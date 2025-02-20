d87b6a8261733a44e1f682969b57d4ce1440fafd
LlLOLaFlLl <93799204+LlLOLaFlLl@users.noreply.github.com>
2025-02-20 10:54:17 +0300

    V_0.1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const items = document.querySelectorAll(".accordion-item");
 
 items.forEach(item => {
   item.addEventListener("click", () => {
-    if (item.classList.contains("active")) { //если секция активна
+    if (item.classList.contains("active")) { //еси секция активна
       item.classList.remove("active"); // закрываем секцию
     } else {
       items.forEach(i => i.classList.remove("active")); // закрываем все неактивные секции
